6c77a7ccf38f247c3760585753a9e093915451ab
jimfoxes <jimfoxes21@yandex.ru>
2024-11-03 03:51:36 +0500

    Создана вторая игра Простая арифметика и добавлена на сайт

--- simple_arithmetic.js
+++ simple_arithmetic.js
@@ -1,5 +1,54 @@
-let numberA = Math.floor(Math.random() * 100) + 1;
-let numberB = Math.floor(Math.random() * 100) + 1;
 
-const operators = ['+', '-', '*', '/'];
 
+function simpleArithmetic() {
+    let numberA = Math.floor(Math.random() * 100) + 1;
+    let numberB = Math.floor(Math.random() * 100) + 1;
+
+    const operators = ['+', '-', '*', '/'];
+
+    let randomIndexOperator = Math.floor(Math.random() * (operators.length));
+    if (typeof operators[randomIndexOperator] === 'string' && /[/]/.test(operators[randomIndexOperator])) {
+        taskResult = Math.round(eval(`${numberA} ${operators[randomIndexOperator]} ${numberB}`)*100) / 100;
+        userResult = prompt(`Привет, реши задачку!\n${numberA} ${operators[randomIndexOperator]} ${numberB}\nЕсли число получается не целым, то округлите до сотых,\nв качестве разделителя используйте точку.`);
+        if (taskResult == userResult) {
+            correctUserResult = confirm(`${userResult} это правильное решение! Желаете ли решить что-нибудь ещё?`);
+            if (correctUserResult == true) {
+                simpleArithmetic();
+            } else {
+                console.log(taskResult);;
+            }
+        } else {
+            incorrectUserResult = confirm(`Неверно\nПравильный ответ ${taskResult}\nЖелаете ли попробовать решить что-нибудь ещё?`);
+            if (incorrectUserResult == true) {
+                simpleArithmetic();
+            } else {
+                console.log(taskResult);;
+            }
+        } 
+    } else if (typeof operators[randomIndexOperator] === 'string' && /[+\-\*]/.test(operators[randomIndexOperator])) {
+        taskResult = eval(`${numberA} ${operators[randomIndexOperator]} ${numberB}`);
+        userResult = prompt(`Привет, реши задачку!\n ${numberA} ${operators[randomIndexOperator]} ${numberB}`);
+            if (taskResult == userResult) {
+                correctUserResult = confirm(`${userResult} это правильное решение! Желаете ли решить что-нибудь ещё?`);
+                if (correctUserResult == true) {
+                    simpleArithmetic();
+                } else {
+                    console.log(taskResult);;
+                }
+            } else {
+                incorrectUserResult = confirm(`Неверно\nПравильный ответ ${taskResult}\nЖелаете ли попробовать решить что-нибудь ещё?`);
+                if (incorrectUserResult == true) {
+                    simpleArithmetic();
+                } else {
+                    console.log(taskResult);;
+                }
+            } 
+    } else {
+        console.log('error');
+    }
+
+    console.log(`${numberA} ${operators[randomIndexOperator]} ${numberB}`);
+    
+}
+
+simpleArithmetic();
